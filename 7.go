6aa09ee6b762e6cb4a5cfd857823e3f0fde0c355
uchitsa <uchitsa@ya.ru>
2024-05-05 02:49:30 +0300

    update 7

--- 7.go
+++ 7.go
@@ -4,7 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strings"
 )
+
+type client struct {
+	wnd int
+	id  int
+	act string
+}
 
 func main() {
 	in := bufio.NewReader(os.Stdin)
@@ -12,45 +20,82 @@ func main() {
 	defer out.Flush()
 
 	var t int
-	fmt.Fscan(in, &t)
+	fmt.Fscanln(in, &t)
 
-	for i := 0; i < t; i++ {
+	for ti := 0; ti < t; ti++ {
 		var n, m int
-		fmt.Fscan(in, &n, &m)
+		fmt.Fscanln(in, &n, &m)
 
-		windows := make([]int, m)
-		for j := 0; j < m; j++ {
-			fmt.Fscan(in, &windows[j])
+		cleints := make([]client, m)
+		for mi := 0; mi < m; mi++ {
+			var w int
+			fmt.Fscan(in, &w)
+			cleints[mi] = client{
+				wnd: w,
+				id:  mi,
+			}
+		}
+		fmt.Fscanln(in)
+
+		res, err := updateClients(cleints, n)
+		if err != nil {
+			fmt.Fprintln(out, "x")
+			continue
 		}
 
-		result := solve(n, windows)
-		fmt.Fprintln(out, result)
+		fmt.Fprintln(out, res)
 	}
 }
 
-func solve(n int, windows []int) string {
-	occupied := make([]bool, n+1)
-	result := make([]byte, len(windows))
-
-	for i, window := range windows {
-		if occupied[window] {
-			return "x"
-		}
-		occupied[window] = true
-		result[i] = '0'
+func updateClients(clients []client, n int) (string, error) {
+	if len(clients) > n {
+		return "", fmt.Errorf("too many cleints")
 	}
 
-	for i, window := range windows {
-		if window > 1 && !occupied[window-1] {
-			result[i] = '-'
-			occupied[window-1] = true
-			occupied[window] = false
-		} else if window < n && !occupied[window+1] {
-			result[i] = '+'
-			occupied[window+1] = true
-			occupied[window] = false
-		}
+	sort.Slice(clients, func(i, j int) bool {
+		return clients[i].wnd < clients[j].wnd
+	})
+
+	if clients[0].wnd > 1 {
+		clients[0].wnd--
+		clients[0].act = "-"
+	} else {
+		clients[0].act = "0"
 	}
 
-	return string(result)
+	for i := 1; i < len(clients); i++ {
+		if (clients[i].wnd - clients[i-1].wnd) == 1 {
+			clients[i].act = "0"
+		}
+
+		if (clients[i].wnd - clients[i-1].wnd) > 1 {
+			clients[i].wnd--
+			clients[i].act = "-"
+		}
+
+		if (clients[i].wnd - clients[i-1].wnd) == 0 {
+			clients[i].wnd++
+			clients[i].act = "+"
+		}
+
+		if (clients[i].wnd - clients[i-1].wnd) < 0 {
+			return "", fmt.Errorf("update is not possible")
+		}
+
+		if clients[i].wnd > n {
+			return "", fmt.Errorf("update is not possible")
+		}
+
+	}
+
+	sort.Slice(clients, func(i, j int) bool {
+		return clients[i].id < clients[j].id
+	})
+
+	sb := strings.Builder{}
+	for _, p := range clients {
+		sb.WriteString(p.act)
+	}
+
+	return sb.String(), nil
 }
